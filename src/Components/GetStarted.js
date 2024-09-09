39326a8c94c88e1674744e48933e39147951440a
MitchellHCrane <mitchellhcrane@gmail.com>
2024-09-09 10:46:57 -0600

    rmv swag

--- src/Components/GetStarted.js
+++ src/Components/GetStarted.js
@@ -20,7 +20,7 @@ function GetStarted() {
           iconName="applicationIcon"
           btnText="Start Application"
         />
-        
+
         <GetStartedItem
           title="CreditSmart Loan Education"
           description="Be confident about your finances. Our lessons will empower you to make educated decisions throughout the homebuying process."
@@ -36,13 +36,13 @@ function GetStarted() {
           iconName="uploadDocumentIcon"
           btnText="Upload Documents"
         />
-        <GetStartedItem
+        {/* <GetStartedItem
           title="Get Free Swag"
           description="Thank you for choosing First Class Home Mortgage to be a part of your home buying experience. Fill out a simple form and get swag!"
           url="https://elevatepromo.com/fchm_thankyou/"
           iconName="peaceHand"
           btnText="Get Swag"
-        />
+        /> */}
         <GetStartedItem
           title="Leave a Review"
           description="Thank you for choosing First Class Home Mortgage. It’s our priority to continue providing quality service to our customers. Tell us about your experience."
